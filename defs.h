fb8e0588993bfc52204ac3351386920ff46317b0
Hadi Nazifi <nazifi_hadi@yahoo.com>
2020-05-07 12:19:20 -0700

    quantum definition added

--- defs.h
+++ defs.h
@@ -121,6 +121,8 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 void            getchildren(int);
+void            clearTick(void);
+int             tickerCount(void);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
